24af07c007b3589bd54cf8b45fb55c4b2dc63017
Brian Emilius <brian@brianemilius.com>
2019-10-12 13:55:46 +0200

    add view engine config to index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import server from "./server"
+import viewEngine from "./config/view-engine"
 import { readdirSync } from "fs"
 import { join } from "path"
 const app = express(),
 			router = express.Router()
+
+viewEngine(app)
 
 readdirSync(join(__dirname, "routes")).forEach(function(file) {
 	require(join(__dirname, "routes", file))(router)
