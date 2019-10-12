aa044b7316d520b9a8a98c0f188bfff66e44fa67
Brian Emilius <brian@brianemilius.com>
2019-10-12 13:55:30 +0200

    change to res.render

--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -2,6 +2,6 @@ import e from "express"
 
 module.exports = function(router: e.Router) {
 	router.get("/", function(req, res, next) {
-		res.send("Hello, World!")
+		res.render("page", { title: "Hello, World!" })
 	})
 }
